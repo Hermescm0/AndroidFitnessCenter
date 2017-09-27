907343d3017466fee063c1177d3cd64001c9a68e
Kevin Madrid <kevin.madrid@cetys.edu.mx>
2017-09-27 10:45:21 -0700

    + News Model

---
+++ app/src/main/java/com/example/razer/fitnesscenter/NewsModel.java
@@ -0,0 +1,12 @@
+package com.example.razer.fitnesscenter;
+
+/**
+ * Created by Lenovo on 9/27/2017.
+ */
+
+public class NewsModel {
+
+    public String title;
+    public String content;
+    public String urlImage;
+}
