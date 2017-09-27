e6b5c4170c734589150bdd1fae90dafc2938b9d7
Kevin Madrid <kevin.madrid@cetys.edu.mx>
2017-09-27 15:26:39 -0700

    ! Errors on Main Fixed

--- app/src/main/java/com/example/razer/fitnesscenter/Font.java
+++ app/src/main/java/com/example/razer/fitnesscenter/Font.java
@@ -1,8 +1,12 @@
 package com.example.razer.fitnesscenter;
 
+import android.graphics.Typeface;
+
 /**
  * Created by Lenovo on 9/27/2017.
  */
 
 public class Font {
+
+
 }
--- app/src/main/java/com/example/razer/fitnesscenter/MainController.java
+++ app/src/main/java/com/example/razer/fitnesscenter/MainController.java
@@ -19,25 +19,6 @@ public class MainController extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         btnLogin = (Button) findViewById(R.id.menu_button_login);
-        btnLogin.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/Roboto-Thin.ttf"));
-        btnRegister = (Button) findViewById(R.id.menu_button_register);
-
-    }
-
-//Abre Register Activity
-    public void openRegister(View view) {
-
-        btnRegister.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent MyIntent = new Intent(MainController.this, RegisterController.class);
-                startActivity(MyIntent);
-            }
-        });
-
-    }
-
-    public void openNews(View view) {
         btnLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +27,16 @@ public class MainController extends AppCompatActivity {
             }
         });
 
+        btnRegister = (Button) findViewById(R.id.menu_button_register);
+
+        btnRegister.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MyIntent = new Intent(MainController.this, RegisterController.class);
+                startActivity(MyIntent);
+            }
+        });
     }
+
 
 }
